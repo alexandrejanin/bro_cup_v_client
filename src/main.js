3f95f6a0da5a148ac7bca7e6c4cb2b268c73e5c5
Alexandre Janin <alexandrejanin92@gmail.com>
2024-06-19 20:20:39 +0200

    http redirect

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import {createPinia} from "pinia";
 import './twitch.v1.js';
 
+if (location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
+
 const app = createApp(App);
 app.use(createPinia());
 app.mount('#app');
